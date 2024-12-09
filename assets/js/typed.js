f2b1057f70488723d101a5dfc1aea8f82d9d008a
Dead <dawdawdawd02@gmail.com>
2024-12-09 19:38:41 +1400

    Add files via upload

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+    strings : [,'Biolink ig','Screams.cc >>>','who are you?','why am i here','#catchin felonys like pokimon'],
+    typeSpeed : 35,
+    delaySpeed : 90,
+    showCursor: false,
+    loop : true
+  })
